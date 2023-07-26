13e93bc1a587165e99d11a2223363d11b8b1ba97
Arnold Chris <arnoldchrisoduor@gmail.com>
2023-07-27 01:59:01 +0300

    first gui version

--- main.py
+++ main.py
@@ -1,40 +1,56 @@
 import random
 import string
+import tkinter as tk
+from tkinter import messagebox
 
 def generate_password(length=12, use_lowercase=True, use_uppercase=True, use_digits=True, use_special=True):
-    characters = ''
-    if use_lowercase:
-        characters += string.ascii_lowercase
-    if use_uppercase:
-        characters += string.ascii_uppercase
-    if use_digits:
-        characters += string.digits
-    if use_special:
-        characters += string.punctuation
+    # Password generation logic (same as previous code)
+    pass
 
-    if not characters:
-        print("Error: Please select at least one character type.")
-        return None
+class PasswordGeneratorApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Password Generator")
+        self.geometry("400x250")
+        self.create_widgets()
 
-    password = ''.join(random.choice(characters) for _ in range(length))
-    return password
+    def create_widgets(self):
+        tk.Label(self, text="Password Generator", font=("Helvetica", 20)).pack(pady=10)
 
-if __name__ == "__main__":
-    print("Password Generator Application")
-    print("-------------------------------")
+        self.length_label = tk.Label(self, text="Password Length:")
+        self.length_label.pack()
+        self.length_entry = tk.Entry(self)
+        self.length_entry.insert(0, "12")  # Default password length
+        self.length_entry.pack()
 
-    while True:
+        self.use_lowercase_var = tk.BooleanVar(value=True)
+        self.use_uppercase_var = tk.BooleanVar(value=True)
+        self.use_digits_var = tk.BooleanVar(value=True)
+        self.use_special_var = tk.BooleanVar(value=True)
+
+        tk.Checkbutton(self, text="Include lowercase letters", variable=self.use_lowercase_var).pack(anchor="w")
+        tk.Checkbutton(self, text="Include uppercase letters", variable=self.use_uppercase_var).pack(anchor="w")
+        tk.Checkbutton(self, text="Include digits", variable=self.use_digits_var).pack(anchor="w")
+        tk.Checkbutton(self, text="Include special characters", variable=self.use_special_var).pack(anchor="w")
+
+        self.generate_button = tk.Button(self, text="Generate Password", command=self.generate_password)
+        self.generate_button.pack(pady=20)
+
+    def generate_password(self):
         try:
-            length = int(input("Enter the desired length of the password: "))
-            use_lowercase = input("Include lowercase letters? (y/n): ").lower() == 'y'
-            use_uppercase = input("Include uppercase letters? (y/n): ").lower() == 'y'
-            use_digits = input("Include digits? (y/n): ").lower() == 'y'
-            use_special = input("Include special characters? (y/n): ").lower() == 'y'
+            length = int(self.length_entry.get())
+            use_lowercase = self.use_lowercase_var.get()
+            use_uppercase = self.use_uppercase_var.get()
+            use_digits = self.use_digits_var.get()
+            use_special = self.use_special_var.get()
 
             password = generate_password(length, use_lowercase, use_uppercase, use_digits, use_special)
 
             if password:
-                print("Generated Password:", password)
-                break
+                messagebox.showinfo("Generated Password", f"Your password is: {password}")
         except ValueError:
-            print("Invalid input. Please enter a valid number for the password length.")
+            messagebox.showerror("Error", "Invalid input. Please enter a valid number for the password length.")
+
+if __name__ == "__main__":
+    app = PasswordGeneratorApp()
+    app.mainloop()
